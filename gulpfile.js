69facff466892a6ea673697548479da4479bbc14
henrykorir <longkhery@gmail.com>
2020-09-26 23:29:44 +0300

    added gulp and babel

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,14 @@ gulp.task('processJS', () => {
 		.pipe(jshint({
 			esversion: 6
 		}))
+		.pipe(jshint.reporter('default'))
+		.pipe(babel({
+			presets: ['env']
+		}))
 		.pipe(gulp.dest('dest'));
 });
 
+gulp.task('babelPolyfill', () => {
+	gulp.src('node_modules/babel-polyfill/browser.js')
+		.pipe(gulp.dest('dist/node_modules/babel-polyfill'));
+});
